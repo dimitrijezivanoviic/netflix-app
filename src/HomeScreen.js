db8a647208a733c994c3638e9c801211cc847341
Dimitrije <zivanovic2601@gmail.com>
2022-01-12 23:29:20 +0100

    Navbar

---
+++ src/HomeScreen.js
@@ -0,0 +1,33 @@
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGw_ksc4XXBVtDAkj-_Mc-lfWJgFdDUuI",
+  authDomain: "netflix-clone-9fc77.firebaseapp.com",
+  projectId: "netflix-clone-9fc77",
+  storageBucket: "netflix-clone-9fc77.appspot.com",
+  messagingSenderId: "210372332595",
+  appId: "1:210372332595:web:61ab981f36a8a51476e746",
+};
