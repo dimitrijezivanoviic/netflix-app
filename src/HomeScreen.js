10300ea84edb411633af8efa555a430d17bb841b
Dimitrije <zivanovic2601@gmail.com>
2022-01-13 00:57:57 +0100

    Displaying movies

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,28 @@
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./requests";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   );
 };
