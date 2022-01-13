af8235f9748598052020f0c23c806c0c93ed10d2
Dimitrije <zivanovic2601@gmail.com>
2022-01-13 19:07:10 +0100

    Adding Sign Up functionality

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,7 +3,7 @@ import SignUpScreen from "./SignUpScreen";
 import "./LoginScreen.css";
 
 function LoginScreen() {
-  const [signIn, setSignIn] = useState(false);
+  const [formUp, setformUp] = useState(false);
 
   return (
     <div className="loginScreen">
@@ -13,28 +13,28 @@ function LoginScreen() {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button onClick={() => setSignIn(true)} className="loginSreen__button">
+        <button onClick={() => setformUp(true)} className="loginSreen__button">
           Sign In
         </button>
 
         <div className="loginScreen__gradient" />
       </div>
       <div className="loginScreen__body">
-        {signIn ? (
-          <SignUpScreen />
+        {formUp ? (
+          <SignUpScreen formUp={formUp} />
         ) : (
           <>
             <h1>Unlimited movies, TV shows, and more.</h1>
             <h2>Watch anywhere. Cancel at any time.</h2>
-            <h3>
+            {/* <h3>
               Ready to watch? Enter your email to create or restart your
               membership.
-            </h3>
+            </h3> */}
             <div className="loginScreen__input">
               <form>
-                <input type="email" placeholder="Email Address" />
+                {/* <input type="email" placeholder="Email Address" /> */}
                 <button
-                  onClick={() => setSignIn(true)}
+                  onClick={() => setformUp(true)}
                   className="loginScreen__getStarted"
                 >
                   GET STARTED
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
@@ -6,7 +6,9 @@ import {
 } from "firebase/auth";
 import "./SignUpScreen.css";
 
-function SignUpScreen() {
+function SignUpScreen({ formUp }) {
+  const [signUpForm, setSignUpForm] = useState(false);
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
@@ -18,9 +20,10 @@ function SignUpScreen() {
         emailRef.current.value,
         passwordRef.current.value
       );
+      alert("Successfull registration.");
       console.log(user);
     } catch (error) {
-      console.log(error.message);
+      alert(error.message);
     }
   };
 
@@ -32,28 +35,43 @@ function SignUpScreen() {
         emailRef.current.value,
         passwordRef.current.value
       );
+      alert("Successfull login.");
       console.log(user);
     } catch (error) {
-      console.log(error.message);
+      alert(error.message);
     }
   };
+
+  const signInJSX = (
+    <h4>
+      <span className="signupScreen__gray"> Already have account?</span>
+      <span className="signupScreen__link" onClick={() => setSignUpForm(false)}>
+        {" "}
+        Log In.
+      </span>
+    </h4>
+  );
+
+  const signUpJSX = (
+    <h4>
+      <span className="signupScreen__gray"> New to Netflix?</span>
+      <span className="signupScreen__link" onClick={() => setSignUpForm(true)}>
+        {" "}
+        Sign Up now.
+      </span>
+    </h4>
+  );
 
   return (
     <div className="signupScreen">
       <form>
-        <h1>Sign In</h1>
+        <h1>{signUpForm ? "Sign Up" : "Sign In"}</h1>
         <input placeholder="Email" type="email" ref={emailRef} />
         <input placeholder="Password" type="password" ref={passwordRef} />
-        <button type="submit" onClick={signIn}>
-          Sign In
+        <button type="submit" onClick={signUpForm ? register : signIn}>
+          {signUpForm ? "Register" : "Login"}
         </button>
-        <h4>
-          <span className="signupScreen__gray"> New to Netflix?</span>
-          <span className="signupScreen__link" onClick={register}>
-            {" "}
-            Sign Up now.
-          </span>
-        </h4>
+        {!signUpForm ? signUpJSX : signInJSX}
       </form>
     </div>
   );
